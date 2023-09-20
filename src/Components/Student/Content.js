ba08090ecaa78f16539c774a441cb62953d6a3a8
harsh <harshitpandeyfeelings@gmail.com>
2023-09-20 23:30:01 +0530

    head name changed

--- src/Components/Student/Content.js
+++ src/Components/Student/Content.js
@@ -6,14 +6,13 @@ import Issue from './Issue';
 import Profile from './Profile';
 import Tab from './Tab';
 
-function changemode(color){
-  const content=document.getElementsByClassName('content');
-  content.style.color=color;
-}
+
 
 const Content = () => {
   
   const [Option,setOption]=useState("Home");
+
+  
 
   function change_board(type)
   {
@@ -37,11 +36,8 @@ const Content = () => {
         </div>
       </div>
       <div className="Dashboard">
-        <div className="Tab">
-          <label> <img src="./img/dark-mode.jpg" onClick={()=>{changemode("yellow")}}/> </label>
-          <label> <img src="./img/mode-light.png" style={{height:"30px",width:"30px"}}/> </label>
-        </div>
-        <div className="content">
+        <Tab/>
+        <div id="content">
           {(Option==="Home") && <Home/>}
           {(Option==="Notice") && <Notice/>}
           {(Option==="Issue") && <Issue/>}
--- src/Components/Student/Tab.js
+++ src/Components/Student/Tab.js
@@ -1,16 +1,26 @@
 import React from 'react'
 
 function changemode(color){
-    const content=document.getElementsByClassName('content');
-    content.style.color=color;
-}
+    const content=document.getElementById('content');
+    content.style.backgroundColor=color;
+    if(color=='#282c34')
+    {
+      content.style.color="white";
+    }
+    else{
+      
+      content.style.color="black"; 
+    }
+    
+  }
 
 const Tab = () => {
     
     return (
         <div className="Tab">
-            <label> <img src="./img/dark-mode.jpg" onClick={()=>{changemode("yellow")}}/> </label>
-            <label> <img src="./img/mode-light.png" style={{height:"30px",width:"30px"}}/> </label>
+          <label> <img src="./img/dark-mode.jpg" onClick={()=>{changemode("#282c34")}}/> </label>
+          <label> <img src="./img/mode-light.png" style={{height:"30px",width:"30px"}} onClick={()=>{changemode("white")}}/> </label>
+          <label> <img src="./img/aqua.png" onClick={()=>{changemode("aqua")}}/> </label>
         </div>
     )
 }

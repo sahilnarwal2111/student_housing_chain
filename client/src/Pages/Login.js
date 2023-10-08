95ae062d5a1282516791f76e9b1fed1d50f6d90c
harsh <harshitpandeyfeelings@gmail.com>
2023-10-08 19:16:40 +0530

    Login Page completed, till fetching data

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,15 +1,54 @@
 import React from 'react';
+import {useState} from 'react';
 import '../css/login.css';
+
+
+
 const Login = () => {
-  return (
+    const [org,setOrg]=useState("try");
+    const [hostel,setHostel]=useState();
+    
+    
+    function checkOrg(event)
+    {
+        console.log(event.target.value);
+        setOrg(event.target.value);
+    }
+    function submit(e)
+    {
+        e.preventDefault();
+        console.log("submit chicked...");
+    }
+    function checkHostel(event)
+    {
+        console.log(event.target.value);
+    }
+
+
+    return (
     <div className="container">
         <div className="login">
-            <form>
-                <input id="org" type="text" placeholder="Select Organization Name"/>
-                <input id="hostel" type="text" placeholder="Select Hostel ID"/>
-                <input id="id" type="text" placeholder="User ID"/>
-                <input id="pass" type="password" placeholder="Password"/>
-            </form>
+            <div className='form' onSubmit={submit}>
+                <h1 className="startHeader">Student Housing Chain</h1>
+                <form className='loginInputs' action={()=>submit()}>
+                    <div>
+                        <input id="org" type="text" onChange={checkOrg} placeholder="Enter Organization Name"/>
+                        <select id="secOrg"></select>
+                    </div>
+                    <div>
+                        <input id="hostel" type="text" onChange={checkHostel} placeholder="Enter Hostel ID"/>
+                        <select id="secHostel">
+                        </select>
+                    </div>
+                    <div>
+                        <input id="userId" type="text" placeholder="User ID"/>
+                    </div>
+                    <div>
+                    <input id="pass" type="password" placeholder="Password"/>
+                    </div>
+                    <input type="submit" id="submit" placeholder="Submit"/>
+                </form>
+            </div>
         </div>
     </div>
   )

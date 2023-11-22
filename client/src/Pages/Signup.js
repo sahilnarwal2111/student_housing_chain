5df53a50701f3b1606b552a870c98f42ee4a4e62
harsh <harshgamingpb@gmail.com>
2023-11-22 16:10:11 +0530

    temporary admin login complete

--- client/src/Pages/Signup.js
+++ client/src/Pages/Signup.js
@@ -5,28 +5,44 @@ import {Link,useNavigate} from 'react-router-dom';
 import Admin from './Admin';
 import { DetailsContext } from '../Components/Details';
 
+var organization_data=[];
+console.log("fetching data...")
+fetch("http://localhost:5000/getDetails",{
+    method:'GET' 
+}).then(
+    response => response.json()
+).then(
+    data => {
+        organization_data=data;
+        console.log(organization_data);
+    }
+)
+
 const Signup = () => {
     const navigate=useNavigate();
     const [org,setOrg]=useState([]);
     const [newOrg,setnewOrg]=useState(false);
     const [invalid,setInvalid]=useState();
     const {setDetails}=useContext(DetailsContext);
-    function getAllOrg()
-    {
-        // import org from backend
-    }
     function checkOrg(event)
     {
         console.log(event.target.value);
         console.log(newOrg);
-        if(event.target.value === "harshit" || event.target.value === "")
+        let input=event.target.value;
+        if(input === "") 
         {
             setnewOrg(false);
+            return;
         }
-        else 
+        for(let i=0;i<organization_data.length;i++)
         {
-            setnewOrg(true);
+            if(organization_data[i].Name === input)
+            {
+                setnewOrg(false);
+                return;
+            }
         }
+        setnewOrg(true);
         
     }
     function Submit(e)
@@ -95,7 +111,7 @@ const Signup = () => {
                 <form className='loginInputs' onSubmit={Submit}>
                         <label><h3><u>Sign Up</u></h3></label>
                         <input id="org" type="text" placeholder="Enter Organization Name" onChange={checkOrg}/>
-                        <p style={{fontSize:"small",marginTop:"-10px"}}>
+                        <p style={{fontSize:"small",marginBottom:"-10px"}}>
                         {newOrg===false && (<b style={{color:"red"}}> [X] Organization Name Unavailable Currently</b>)}
                         {newOrg===true && (<b style={{color:"brown"}}> [O] Organzation Name Available</b>)}
                         </p>

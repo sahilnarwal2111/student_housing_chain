ea0a37e2278eb3e0988a70e795b07fc0a9612421
harsh <harshgamingpb@gmail.com>
2023-12-06 23:21:27 +0530

    imporved layout and changed site color

---
+++ client/src/Components/Student/Home.js
@@ -0,0 +1,16 @@
+import React,{useState,useContext} from 'react';
+import DataBox from '../DataBox';
+import {DetailsContext} from '../Details';
+const Home = () => {
+  const {details} = useContext(DetailsContext);
+  const [temp_details,setTempDetails]=useState(details);
+  return (
+    <>
+      {console.log("home")}
+      {console.log(details)}
+      {(details !== "") && <DataBox dataspread={true} del={false} data={details.appeal}/>}
+    </>
+  )
+}
+
+export default Home
--- client/src/Components/Tab.js
+++ client/src/Components/Tab.js
@@ -1,27 +1,30 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
-function changemode(color){
-    const content=document.getElementById('content');
-    content.style.backgroundColor=color;
-    if(color==='#282c34')
-    {
-      content.style.color="white";
-    }
-    else{
-      
-      content.style.color="black"; 
-    }
-    
-  }
+
 
 const Tab = (props) => {
-    
+    const [darkMode,setDarkMode]=useState(false);
+      useEffect(()=>{
+
+          const content=document.getElementById('content');
+          if(!darkMode)
+          {
+            content.style.background="white";
+            content.style.color="black";
+          }
+          else{
+            content.style.background="#282c34";
+            content.style.color="white"; 
+          }
+        }
+      ,[darkMode]);
+      
+
     return (
         <div className="Tab">
           <label> {props.type} </label>
-          <label> <img src="./img/dark-mode.jpg" alt="" onClick={()=>{changemode("#282c34")}}/> </label>
-          <label> <img src="./img/mode-light.png" alt="" style={{height:"30px",width:"30px"}} onClick={()=>{changemode("white")}}/> </label>
-          <label> <img src="./img/aqua.png" alt="" onClick={()=>{changemode("aqua")}}/> </label>
+          <label> <img src="./img/dark-mode.jpg" alt="" onClick={()=>{setDarkMode(true)}}/> </label>
+          <label> <img src="./img/mode-light.png" alt="" onClick={()=>{setDarkMode(false)}}/> </label>
         </div>
     )
 }

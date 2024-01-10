9c79e166dffd1ce7ce8d1d417ba627c6cce942d7
harsh <harshgamingpb@gmail.com>
2024-01-10 12:57:34 +0530

    adding invalid

--- client/src/Pages/Admin.js
+++ client/src/Pages/Admin.js
@@ -1,13 +1,14 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useState,useContext, useEffect } from 'react'
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
 import Content from '../Components/Admin/Content';
 import { DetailsContext } from '../Components/Details';
+import Invalid from './Invalid';
 const Admin = () => {
 
   const { details } = useContext(DetailsContext);
   const { setDetails } = useContext(DetailsContext);
-
+  //const [error,setError] = useState(false);
 
   useEffect(() => {
     function getData() {
@@ -28,14 +29,15 @@ const Admin = () => {
             let temp_details = data._doc;
 
             if (temp_details !== null && temp_details !== undefined) { setDetails({ ...temp_details }) }
+            //setError(false)
           }
         ).catch(
-          err => console.log(err)
+          //setError(true)
         )
     }
     setInterval(getData, 10000)
   }, []);
-
+  //if(error) return <Invalid/>
   return (
     <div className="container">
       <Header />
